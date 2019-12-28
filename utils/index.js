343c875aba87ceef162c64883f67c90293ac8a2f
qin <qinsong007@gmail.com>
2019-12-28 10:48:20 +0800

    feat: 新增函数节流、函数防抖

--- utils/index.js
+++ utils/index.js
@@ -129,3 +129,27 @@ function setCursorPosition(elem, index) {
     }
   }, 10);
 }
+
+//函数防抖
+function debounce(fn, interval = 300) {
+  let timeout = null;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, interval);
+  };
+}
+
+// 函数节流
+function throttle(fn, interval = 300) {
+  let canRun = true;
+  return function() {
+    if (!canRun) return;
+    canRun = false;
+    setTimeout(() => {
+      fn.apply(this, arguments);
+      canRun = true;
+    }, interval);
+  };
+}
