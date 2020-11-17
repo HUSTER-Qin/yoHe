3a1da0a51d00921ab961c1ba4b4f3d604561774f
qin <qinsong1@tal.com>
2020-11-18 01:10:54 +0800

    feat: 面试1.0

--- algorithms/utils/sort.ts
+++ algorithms/utils/sort.ts
@@ -23,3 +23,34 @@ function bubbleSort(arr: Array<number>): Array<number> {
 }
 
 bubbleSort([9, 1, 4, 2, 8, 3, 1, 3, 7]);
+
+// 选择排序
+// 时间复杂度 O(N2)
+// 所以用到它的时候，数据规模越小越好。唯一的好处可能就是不占用额外的内存空间了吧。
+
+// 1. 算法步骤
+// 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+
+// 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+
+// 重复第二步，直到所有元素均排序完毕。
+
+function selectionSort(nums: Array<number>): Array<number> {
+  let min: number = 0;
+  for (let j = 0; j < nums.length; j++) {
+    min = j
+    for (let i = j; i < nums.length; i++) {
+      if (nums[min] > nums[i]) {
+        min = i;
+      }
+    }
+    let temp = nums[j];
+    nums[j] = nums[min];
+    nums[min] = temp;
+  }
+  console.log(nums);
+  
+  return nums;
+}
+
+selectionSort([9, 1, 4, 2, 8, 3, 1, 3, 7]);
