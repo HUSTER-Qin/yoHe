dd8e3808818c31d7b582bd486162e0e57e19f9ab
qin <qinsong1@tal.com>
2021-04-01 08:23:39 +0800

    fix: ..

---
+++ month/2.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+
+    let map1 = new Map()
+    nums1.map((item, index) => map1.set(item, index))
+    
+    
+};
+
+intersect([1, 2, 2, 1], [2])
