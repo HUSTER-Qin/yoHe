01a8d6d5d19cb9f37ddc18e6161acded05332f4a
qin <qinsong007@outlook.com>
2021-04-13 11:46:32 +0800

    feat: 快捷键

---
+++ month/20210412/containsDuplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let result = 0
+    let map  = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) {
+            map.set(nums[i],"")
+        }
+    }
+    return map.size !== nums.length
+};
+
+console.log(containsDuplicate([1,2,3,4]));
---
+++ q.js
@@ -0,0 +1,8 @@
+function foo(a,b,c,d) { 
+    console.log(arguments);
+    var arg = Array.prototype.slice.call(arguments); 
+    console.log(arg); 
+    console.log(typeof arguments);
+    console.log(arguments);
+ } 
+ new foo("a","b","c","d"); 
