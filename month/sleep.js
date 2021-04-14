c88d279b73f854bafe30759eab05ecb39c565b45
qin <qinsong007@outlook.com>
2021-04-14 16:56:15 +0800

    feat: ..

---
+++ month/sleep.js
@@ -0,0 +1,14 @@
+
+
+function sleep(time){
+    return new Promise((resolve,reject)=>{
+        setTimeout(resolve,time)
+    })
+}
+
+async function  main(){
+    await sleep(3000).then(console.log("red"))
+    await sleep(1000).then(console.log("red"))
+    await sleep(2000).then(console.log("red"))
+}
+main()
